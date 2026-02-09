f3d2a2f7da97a398b45eae1d14fdba0a29d54a7a
WimukthiMadushan <bandarawgwm.21@uom.lk>
2026-02-09 11:49:52 +0530

    change configurations

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,11 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
+  },  preview: {
+    host: "::",
+    port: 8080,
+    allowedHosts: ["frontend-0bfs.onrender.com"],
+  },  plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
   resolve: {
