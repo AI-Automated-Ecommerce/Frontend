fbac77a489c394792b5f2ed16076e8735dc7bc9b
WimukthiMadushan <bandarawgwm.21@uom.lk>
2026-02-09 11:06:59 +0530

    change configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["frontend-0bfs.onrender.com"],
     hmr: {
       overlay: false,
     },
